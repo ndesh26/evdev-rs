864ddf1e4cf7ef7f7d6d88471236bde13f26135d
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2016-12-08 09:43:03 +0530

    use pkg-config crate to link to libevdev
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- evdev-sys/build.rs
+++ evdev-sys/build.rs
@@ -1,5 +1,13 @@
+extern crate pkg_config;
+
 fn main() {
+
+   if let Ok(lib) = pkg_config::find_library("libevdev") {
+        for path in &lib.include_paths {
+            println!("cargo:include={}", path.display());
+        }
+        return
+    }
+
     // TODO: build libevdev ourselves in case it is not present
-    println!("cargo:rustc-link-search=usr/lib64/");
-    println!("cargo:rustc-link-lib=evdev");
 }
