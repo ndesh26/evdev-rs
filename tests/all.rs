e136b7855d115755e060df8517d9fdd90dcbeffd
Max Blachman <blachmanmax@gmail.com>
2020-10-27 00:30:33 -0700

    rename fd methods to file, add UninitDevice
    Methods called something fd or fd something are named confusingly, as
    they often operate on `File`s. This was originally done to
    mantain similarity with libevdev, but violates the least surprise
    principle due to the existence of rust's `RawFd`.
    
    The UninitDevice struct was added to provide compatibility with
    libevdev, which allows uninitialized devices, but is idiomatic
    rust, encoding the initalization in the type system.

--- tests/all.rs
+++ tests/all.rs
@@ -4,57 +4,49 @@ use std::fs::File;
 use std::os::unix::io::AsRawFd;
 
 #[test]
-#[allow(dead_code)]
 fn context_create() {
-    Device::new().unwrap();
+    assert!(UninitDevice::new().is_some());
 }
 
 #[test]
-fn context_create_with_fd() {
+fn context_create_with_file() {
     let f = File::open("/dev/input/event0").unwrap();
-    let _d = Device::new_from_fd(f).unwrap();
+    let _d = Device::new_from_file(f).unwrap();
 }
 
 #[test]
-fn context_set_fd() {
-    let mut d = Device::new().unwrap();
+fn context_set_file() {
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
-
-    match d.set_fd(f) {
-        Ok(()) => ..,
-        Err(result) => panic!("Error {}", result),
-    };
+    let _device = d.set_file(f).unwrap();
 }
 
 #[test]
-fn context_change_fd() {
-    let mut d = Device::new().unwrap();
+fn context_change_file() {
+    let d = UninitDevice::new().unwrap();
     let f1 = File::open("/dev/input/event0").unwrap();
     let f2 = File::open("/dev/input/event0").unwrap();
     let f2_fd = f2.as_raw_fd();
 
-    d.set_fd(f1).unwrap();
-    match d.change_fd(f2) {
-        Ok(()) => ..,
-        Err(result) => panic!("Error {}", result),
-    };
+    let mut d = d.set_file(f1).unwrap();
+    d.change_file(f2).unwrap();
 
-    assert_eq!(d.fd().unwrap().as_raw_fd(), f2_fd);
+    assert_eq!(d.file().as_raw_fd(), f2_fd);
 }
 
 #[test]
 fn context_grab() {
-    let mut d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
 
-    d.set_fd(f).unwrap();
+    let mut d = d.set_file(f).unwrap();
     d.grab(GrabMode::Grab).unwrap();
     d.grab(GrabMode::Ungrab).unwrap();
 }
 
 #[test]
 fn device_get_name() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_name("hello");
     assert_eq!(d.name().unwrap(), "hello");
@@ -62,7 +54,7 @@ fn device_get_name() {
 
 #[test]
 fn device_get_uniq() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_uniq("test");
     assert_eq!(d.uniq().unwrap(), "test");
@@ -70,7 +62,7 @@ fn device_get_uniq() {
 
 #[test]
 fn device_get_phys() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_phys("test");
     assert_eq!(d.phys().unwrap(), "test");
@@ -78,7 +70,7 @@ fn device_get_phys() {
 
 #[test]
 fn device_get_product_id() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_product_id(5);
     assert_eq!(d.product_id(), 5);
@@ -86,7 +78,7 @@ fn device_get_product_id() {
 
 #[test]
 fn device_get_vendor_id() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_vendor_id(5);
     assert_eq!(d.vendor_id(), 5);
@@ -94,7 +86,7 @@ fn device_get_vendor_id() {
 
 #[test]
 fn device_get_bustype() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_bustype(5);
     assert_eq!(d.bustype(), 5);
@@ -102,7 +94,7 @@ fn device_get_bustype() {
 
 #[test]
 fn device_get_version() {
-    let d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
 
     d.set_version(5);
     assert_eq!(d.version(), 5);
@@ -110,10 +102,10 @@ fn device_get_version() {
 
 #[test]
 fn device_get_absinfo() {
-    let mut d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
 
-    d.set_fd(f).unwrap();
+    let d = d.set_file(f).unwrap();
     for code in EventCode::EV_SYN(EV_SYN::SYN_REPORT).iter() {
         let absinfo: Option<AbsInfo> = d.abs_info(&code);
 
@@ -126,10 +118,10 @@ fn device_get_absinfo() {
 
 #[test]
 fn device_has_property() {
-    let mut d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
 
-    d.set_fd(f).unwrap();
+    let d = d.set_file(f).unwrap();
     for prop in InputProp::INPUT_PROP_POINTER.iter() {
         if d.has(&prop) {
             panic!("Prop {} is set, shouldn't be", prop);
@@ -139,10 +131,10 @@ fn device_has_property() {
 
 #[test]
 fn device_has_syn() {
-    let mut d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
 
-    d.set_fd(f).unwrap();
+    let d = d.set_file(f).unwrap();
 
     assert!(d.has(&EventType::EV_SYN)); // EV_SYN
     assert!(d.has(&EventCode::EV_SYN(EV_SYN::SYN_REPORT))); // SYN_REPORT
@@ -150,10 +142,10 @@ fn device_has_syn() {
 
 #[test]
 fn device_get_value() {
-    let mut d = Device::new().unwrap();
+    let d = UninitDevice::new().unwrap();
     let f = File::open("/dev/input/event0").unwrap();
 
-    d.set_fd(f).unwrap();
+    let d = d.set_file(f).unwrap();
 
     let v2 = d.event_value(&EventCode::EV_SYN(EV_SYN::SYN_REPORT)); // SYN_REPORT
     assert_eq!(v2, Some(0));
