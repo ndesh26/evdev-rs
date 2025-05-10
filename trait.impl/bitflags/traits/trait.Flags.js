e6ade51845c753522e5c81e87a3836d40eb7b1a6
ci <ci>
2025-05-10 16:33:33 +0000

    init

---
+++ trait.impl/bitflags/traits/trait.Flags.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["evdev_rs",[["impl Flags for <a class=\"struct\" href=\"evdev_rs/struct.ReadFlag.html\" title=\"struct evdev_rs::ReadFlag\">ReadFlag</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[141]}
